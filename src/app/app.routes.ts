14f0994c98334a160ca577b0e5b99fe93b7d9c0d
CSWilder02 <126688647+CSWilder02@users.noreply.github.com>
2024-03-07 12:59:38 +0200

    Front-end Login
    Front-end login page design is completed and now the functionality should be added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // other routes...
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    // other routes...
+];
