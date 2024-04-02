c1e48345f3d6c7d7061bce67008ab120d3205bc2
CWKrahtz <221198@virtualwindow.co.za>
2024-04-02 14:42:57 +0200

    Created and linked the Backend of users to front end for login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { InventoryComponent } from './components/inventory/inventory.component';
+import { AuthService } from './services/auth.service';
 
 
 @Component({
@@ -12,4 +13,33 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 })
 export class AppComponent {
   title = 'PalCODEX';
+
+  constructor(private service: AuthService){}
+
+  public isLoggedIn = false
+  ngOnInit(){
+    //check if user is logged in
+    this.checkLoginState()
+  }
+
+  checkLoginState(){
+    
+  }
+
 }
+
+//AppComponent is our main entry point
+
+//TODO: 
+//DONE
+//0. Login Component
+//DONE
+//1. Create Our Auth Service - Connect to endpoints
+//DONE
+//1.1 Save users info to the sessionStorage 
+
+//2. Create an Auth Gaurd - protects our routs from invalid users
+
+//3. Create Admin Gaurd - only admin can view crafting
+
+//4. Update the Navigation UI, based on if the user is logged in or not
